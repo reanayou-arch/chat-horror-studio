2d7f542fc4ae6838962d36489cb4ab722228d0ec
reanayou-arch <reanayou@gmail.com>
2026-01-21 04:08:27 +0300

    Update storage.js

--- ja/storage.js
+++ ja/storage.js
@@ -1,1 +1,13 @@
+const STORAGE_KEY = "chat_horror_stories_v1";
 
+export function loadStories() {
+  try {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+  } catch {
+    return [];
+  }
+}
+
+export function saveStories(stories) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(stories));
+}
