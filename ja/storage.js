b644fb689a273cc323e0ae4a072f3afada802e2c
reanayou-arch <reanayou@gmail.com>
2026-01-21 04:21:10 +0300

    Rename STORAGE_KEY to KEY in storage.js

--- ja/storage.js
+++ ja/storage.js
@@ -1,13 +1,13 @@
-const STORAGE_KEY = "chat_horror_stories_v1";
+const KEY = "chat_horror_stories_v1";
 
 export function loadStories() {
   try {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    return JSON.parse(localStorage.getItem(KEY)) || [];
   } catch {
     return [];
   }
 }
 
 export function saveStories(stories) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(stories));
+  localStorage.setItem(KEY, JSON.stringify(stories));
 }
